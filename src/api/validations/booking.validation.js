5bee2cf376b2a06e371c266ed3a6a8c13a45d821
Muhd Saiful <57882573+MuhdSaifulnizam15@users.noreply.github.com>
2024-06-13 23:30:35 +0800

    feat: update booking logic

--- src/api/validations/booking.validation.js
+++ src/api/validations/booking.validation.js
@@ -2,11 +2,13 @@ const Joi = require("joi");
 
 const createBooking = {
   body: Joi.object().keys({
-    name: Joi.string().required(),
+    // name: Joi.string().required(),
     phone_no: Joi.string().required(),
     car_plate: Joi.string().required(),
     status: Joi.string(),
     branch_id: Joi.string().required(),
+    customer_id: Joi.string(),
+    customer_name: Joi.string()
   }),
 };
 
